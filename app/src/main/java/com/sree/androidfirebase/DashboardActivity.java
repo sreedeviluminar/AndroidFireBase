dc372faba29c7676472d3b23838030e09aa171fa
Sreedevi <sreedevi@luminartechnolab.com>
2022-02-19 15:47:10 +0530

    Initial commit

---
+++ app/src/main/java/com/sree/androidfirebase/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.sree.androidfirebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sree.androidfirebase.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
